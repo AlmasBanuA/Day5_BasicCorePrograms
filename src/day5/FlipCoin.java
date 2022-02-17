1fbe40a607dd9343412d3154ad8790cd810f77d7
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 21:43:40 +0530

    Flip Coin and printing percentage of Heads and Tails

--- src/day5/FlipCoin.java
+++ src/day5/FlipCoin.java
@@ -1,18 +1,22 @@
 package day5;
 
-import java.util.Scanner;
+import java.util.Scanner; 		//import scanner class     
 
 public class FlipCoin {
 	
 	public static void main(String[] args) {
 		
-	Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);		//create scanner object
+		
         System.out.println("Enter Number how many times you want to flip a coin");
+		
         int n = sc.nextInt();
         int head =0,tail=0;
         
-        for(int i=0;i<n;i++){
-            if(Math.random()<0.5)
+	//create forloop to check the condition
+	 for(int i=0;i<n;i++){
+		
+            if(Math.random()<0.5) 		//random function 
             {
                 head++;
             }
@@ -22,10 +26,13 @@ public class FlipCoin {
             }
         }
 		
-        int headper = (head*100)/n;
-        int tailper = (tail*100)/n;
+        int headper = (head*100)/n;		//percentage of heads by using formula
+		
+	 int tailper = (tail*100)/n;		//percentage of tails by using formula
+		
         System.out.println("Head Percentage :"+headper+"%");
         System.out.println("Tail Percentage :"+tailper+"%");
+		
         System.out.println(head);
         System.out.println(tail);
     }
