f447c49ab3858f4781043e55e4addfb73e7c0532
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 22:14:57 +0530

    Computing the prime factorization of N

--- src/day5/PrimeFactors.java
+++ src/day5/PrimeFactors.java
@@ -1,21 +1,28 @@
 package day5;
 
-import java.util.Scanner;
+import java.util.Scanner;	//import scanner class
 
 public class PrimeFactors {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);		//create scanner object
+		
 		System.out.println("Enter the number");
-		int n = sc.nextInt();
+		
+		int n = sc.nextInt();		//taking n value from input
+		
 		System.out.println("Prime factors for the given numbers are :");
-		for (int i = 2; i <= n; i++) {
-			while (n % i == 0) {
+		
+		for (int i = 2; i <= n; i++) 		//taking forloop to check condition prime num started with 2
+		{
+			
+		       while (n % i == 0)		//while loop to check condition 
+		       		{
 
-				System.out.println(i + "");
-				n = n / i;
-			}
+					System.out.println(i + "");
+					n = n / i;
+				}
 		}
 
 	}
