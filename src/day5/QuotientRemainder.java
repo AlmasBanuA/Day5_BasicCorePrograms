c26886402257066eb82e88d853359fd4b4f2711b
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 22:20:32 +0530

    Computing Quotient and Remainder

--- src/day5/QuotientRemainder.java
+++ src/day5/QuotientRemainder.java
@@ -1,18 +1,23 @@
 package day5;
 
-import java.util.Scanner;
+import java.util.Scanner;	//import scanner class
 
 public class QuotientRemainder {
 
 	public static void main(String[] args) {
 
-		int num1, num2, quotient, remainder;
-		Scanner sc = new Scanner(System.in);
+		int num1, num2, quotient, remainder;	// declare num values ,quotient and remainder
+		
+		Scanner sc = new Scanner(System.in);	// create scanner object
+		
 		System.out.println("Enter the value of num1 and num2");
-		num1 = sc.nextInt();
+		
+		num1 = sc.nextInt();		// taking num values from user
 		num2 = sc.nextInt();
-		remainder = num1 % num2;
-		quotient = num1 / num2;
+		
+		remainder = num1 % num2;	// modulating two numbers to get remainder
+		
+		quotient = num1 / num2;		// division of two numbers 
 
 		System.out.println("The quotient are:" + quotient);
 		System.out.println("The remainder are:" + remainder);
