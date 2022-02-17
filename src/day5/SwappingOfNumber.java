28d9556802ce041c901c076947c94e0b0c7b3462
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 20:33:00 +0530

    Swapping of Two Numbers

---
+++ src/day5/SwappingOfNumber.java
@@ -0,0 +1,25 @@
+package day5;
+
+import java.util.Scanner;
+
+public class SwappingOfNumber {
+
+	public static void main(String[] args) {
+
+		int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a and b:");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("before swapping no are:"+a+" "+b);
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("After swapping no are:"+a+" "+b);
+        
+	}
+}
+
+	
+
+
