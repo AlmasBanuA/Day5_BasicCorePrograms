14a255ed9f2d8964268da1862c9c278e3d485c55
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 20:37:25 +0530

    Checking Whether a Number is Even or Odd

---
+++ src/day5/EvenOdd.java
@@ -0,0 +1,23 @@
+package day5;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+
+		int num;
+		System.out.println("Enter an integer number:");
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		if (num % 2 == 0)
+			System.out.println("Number is even");
+		else
+			System.out.println("Number is odd");
+	}
+}
+
+
+	
+
+
