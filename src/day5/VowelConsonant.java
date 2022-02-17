579ef2507b77ade2cc18bf23d60879faa71839db
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 22:31:04 +0530

    Checking Whether an Alphabet is Vowel or Consonant

--- src/day5/VowelConsonant.java
+++ src/day5/VowelConsonant.java
@@ -1,18 +1,24 @@
 package day5;
 
-import java.util.Scanner;
+import java.util.Scanner;	//import scanner class
 
 public class VowelConsonant {
 
 	public static void main(String[] args) {
 
-		char ch;
+		char ch;	// create ch variable
+		
 		System.out.println("Enter a character:");
-		Scanner sc = new Scanner(System.in);
+		
+		Scanner sc = new Scanner(System.in);		//create scanner obeject
 		ch = sc.next().charAt(0);
 
+		
+		// using ifelse statement to check alphabet is vowel or not
 		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
-				|| ch == 'O' || ch == 'U') {
+				|| ch == 'O' || ch == 'U') 
+			
+		{
 			System.out.println("the given char is vowel");
 		} else
 			System.out.println("The given char is consonant");
