3ca60ebe7dde44c8555330b921b3dba26ad0f537
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 22:05:24 +0530

    Printing the table of power of two

--- src/day5/PowerOfTwo.java
+++ src/day5/PowerOfTwo.java
@@ -1,21 +1,29 @@
 package day5;
 
-import java.util.Scanner;
+import java.util.Scanner;		//import scanner class
 
 public class PowerOfTwo {
 
 	public static void main(String[] args) {
 
-		int num;
+		int num;	//declaring num variable in int data type
+		
 		System.out.println("Enter the number:");
+		
 		Scanner sc = new Scanner(System.in);
-		num = sc.nextInt();
-		if (num > 31) {
+		
+		num = sc.nextInt();		//taking num value from input
+		
+		if (num > 31)
+		{
 			System.out.println("overflow");
-		} else
-			for (int i = 1; i <= num; i++) {
-				System.out.println("Power of 2^" + i + " is :" + (Math.pow(2, i)));
-			}
+		} 
+		
+		else
+		for (int i = 1; i <= num; i++) 
+		{
+			System.out.println("Power of 2^" + i + " is :" + (Math.pow(2, i)));	//using math.power method to print table of power of 2
+		}
 	}
 }
 	
