20ef65b426a23215dd9342acade3531693236e17
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-17 21:32:13 +0530

    Checking Whether a Number is Even or Odd

--- src/day5/EvenOdd.java
+++ src/day5/EvenOdd.java
@@ -1,15 +1,18 @@
 package day5;
 
-import java.util.Scanner;
+import java.util.Scanner; //import scanner class
 
 public class EvenOdd {
 
 	public static void main(String[] args) {
 
-		int num;
-		System.out.println("Enter an integer number:");
-		Scanner sc = new Scanner(System.in);
-		num = sc.nextInt();
+		int num; //declaring num variable
+		System.out.println("Enter an integer number:"); 
+		Scanner sc = new Scanner(System.in); //create scanner class object
+		
+		num = sc.nextInt(); //taking the num value from the user input
+		
+		// using modulo operator to check the number is divisible by 2 or not 
 		if (num % 2 == 0)
 			System.out.println("Number is even");
 		else
